69f525593904ec5654b1254da94a63995511b463
Goodness Daramola <gdaramol@thoughtworks.com>
2018-11-05 20:01:40 -0500

    first deploy

---
+++ src/main/java/com/prodtrak/prodtrakservice/ProdtrakServiceApplication.java
@@ -0,0 +1,12 @@
+package com.prodtrak.prodtrakservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdtrakServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProdtrakServiceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/prodtrak/prodtrakservice/ProdtrakServiceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.prodtrak.prodtrakservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProdtrakServiceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
